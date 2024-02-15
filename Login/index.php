9a4de6a4a9d4315c80bf8b47b1937297f9d7e134
yohanesrickyyy <yohanesrickyyy@gmail.com>
2024-02-16 00:45:07 +0700

    fix bug

--- Login/index.php
+++ Login/index.php
@@ -1,16 +1,13 @@
 <?php
-// errror checking
+// validasi error
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-//! SIGNUP
+// SIGNUP
 if(isset($_POST['submit-su'])) {
     $fname_su = $_POST['fname-su'];
     $email_su = $_POST['email-su'];
     $pw_su = $_POST['pw-su'];
-
-    // Hashing password
-    $hashed_pw_su = password_hash($pw_su, PASSWORD_DEFAULT);
 
     $host = "localhost";
     $username = "root";
@@ -26,27 +23,20 @@ if(isset($_POST['submit-su'])) {
     }
 
     // memasukan data ke sql (signup)
-    $sql_signup = "INSERT INTO data_signup (fullname, email, password) VALUES (?, ?, ?)";
-
-    // Prepared statement
-    $stmt_signup = mysqli_prepare($con, $sql_signup);
-    mysqli_stmt_bind_param($stmt_signup, "sss", $fname_su, $email_su, $hashed_pw_su);
-    mysqli_stmt_execute($stmt_signup);
+    $sql_signup = "INSERT INTO data_signup (fullname, email, password) VALUES ('$fname_su', '$email_su', '$pw_su')";
 
     // validasi jika data berhasil masuk ke sql
-    if(mysqli_stmt_affected_rows($stmt_signup) > 0) {
+    $rs_signup = mysqli_query($con, $sql_signup);
+    if($rs_signup) {
         echo "Data signup telah terinput";
     } else {
         echo "Gagal memasukkan data signup: " . mysqli_error($con);
     }
 
-    // close statement
-    mysqli_stmt_close($stmt_signup);
     // close connection
     mysqli_close($con);
 }
 
-//! LOGIN FORM
 if(isset($_POST['submit-lo'])) {
     $email_lo = $_POST['email-lo'];
     $pw_lo = $_POST['pw-lo'];
@@ -65,23 +55,16 @@ if(isset($_POST['submit-lo'])) {
     }
 
     // memasukan data ke sql (login)
-  $sql_login = "INSERT INTO data_login (email, password) VALUES ('$email_lo', '$pw_lo')";
-
-
-    // Prepared statement
-    $stmt_login = mysqli_prepare($con, $sql_login);
-    mysqli_stmt_bind_param($stmt_login, "ss", $email_lo, $pw_lo);
-    mysqli_stmt_execute($stmt_login);
+    $sql_login = "INSERT INTO data_login (email, password) VALUES ('$email_lo', '$pw_lo')";
 
     // validasi jika data berhasil masuk ke sql
-    if(mysqli_stmt_affected_rows($stmt_login) > 0) {
+    $rs_login = mysqli_query($con, $sql_login);
+    if($rs_login) {
         echo "Data login telah terinput";
     } else {
         echo "Gagal memasukkan data login: " . mysqli_error($con);
     }
 
-    // close statement
-    mysqli_stmt_close($stmt_login);
     // close connection
     mysqli_close($con);
 }
